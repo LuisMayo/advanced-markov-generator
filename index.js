91a72147a231a365dc73e2b491242314dde4ce35
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2022-04-13 12:18:19 +0200

    Changes to make endings probability-based.
    Begginings may be probability-based too

--- index.js
+++ index.js
@@ -5,70 +5,62 @@ class Markov {
    * @param {object} props - The configuration options and input data
    */
   constructor (props) {
-    this.props = props
+    const defaultConfig = {
+      minLength: 10,
+      /**
+       * Defines whether to use probability for the first word. If true more common words on the learning set
+       * are more likely to be the first word. If false all words have equal chance
+       *  */ 
+      probabilityForFirstWord: false
+    }
+
+    this.props = Object.assign(defaultConfig, props);
     if (!this.props.input) {
       throw new Error('input was empty!')
     }
-    this.terminals = {}
     this.startWords = []
     this.wordStats = {}
-    this.bannedTerminals = (props.bannedTerminals && props.bannedTerminals.length) ? props.bannedTerminals.map(word => word.toLowerCase()) : [];
 
-    this.props.input.forEach((e, i, a) => {
-      let words = e.split(' ')
-      let lastWord = words[words.length - 1]
+    this.props.input.forEach((learningElement) => {
+      let words = learningElement.split(' ')
       let firstWord = words[0]
-
-      // if this.terminals contains the last word in this sentence, add to it's counter
-      // otherwise, create the property on this.terminals and set it to 1
-      if (this.terminals[lastWord]) {
-        this.terminals[lastWord]++
-      } else {
-        this.terminals[lastWord] = 1
-      }
 
 
       // this function tests to see if this.startWords already contains the first word or not
       // we can't use Array.prototype.includes, because when comparing each element in this.startWords to the first word, we need to compare them as lowercase
-      let checkWordNotInStartWords = (refWord) =>{
-        this.startWords.forEach((elem, ind, arr) => {
+      let checkWordNotInStartWords = () =>{
+        if (this.props.probabilityForFirstWord) {
+          return true;
+        }
+        this.startWords.forEach((elem) => {
           if (elem.toLowerCase() === firstWord.toLowerCase()) {
-            return false
+            return false;
           }
-        })
-        return true
+        });
+        return true;
       }
 
       // if the first word is not a space, and if this.startWords does not already contain the first word, add it
-      if (firstWord.length && checkWordNotInStartWords(firstWord)) {
+      if (firstWord.length && checkWordNotInStartWords()) {
         this.startWords.push(firstWord)
       }
 
       // loop through each word in current sentence
-      words.forEach((el, it, ar) => {
+      words.forEach((currentWord, it, ar) => {
+        const nextWord = ar[it + 1];
         // if this.wordStats already contains the current word in the sentence as a property, push the next word in the sentence to it's array
         // otherwise, create the property on this.startWords and set it to an array containing the next word in the sentence
         // first check to see if there even IS a next word
         // we store all of the keys in this.wordStats as lowercase to make the function makeChain case insensitive
-        if (ar[it + 1]) {
-          if (this.wordStats.hasOwnProperty(el.toLowerCase())) {
-            this.wordStats[el.toLowerCase()].push(ar[it + 1])
-          } else {
-            this.wordStats[el.toLowerCase()] = [ar[it + 1]]
-          }
+        if (this.wordStats.hasOwnProperty(currentWord.toLowerCase())) {
+          this.wordStats[currentWord.toLowerCase()].push(nextWord)
+        } else {
+          this.wordStats[currentWord.toLowerCase()] = [nextWord]
         }
       })
     })
 
-    for (let word in this.terminals) {
-      if (this.terminals[word] === '' || !this.terminals[word]) { delete this.terminals[word] }
-    }
-    delete this.terminals['']
     delete this.wordStats['']
-  }
-
-  isBannedTerminal(word) {
-    return this.bannedTerminals.includes(word.toLowerCase());
   }
 
   /**
@@ -85,23 +77,23 @@ class Markov {
    * @param {number} minLength - The minimum number of words in the generated string
    * @return {string} The generated string
    */
-  makeChain (minLength = this.props.minLength || 10) {
+  makeChain (minLength = this.props.minLength || 10, deepness = 1) {
+    if (deepness >= 4) {
+      return "Sorry, I'm not able to generate a chain";
+    }
     let word = this.choice(this.startWords);
-    let chain = [word];
+    let chain = [];
 
-    while (this.wordStats.hasOwnProperty(word.toLowerCase())) {
-      let nextWords = this.wordStats[word.toLowerCase()];
-      word = this.choice(nextWords);
+    while (word != null && this.wordStats.hasOwnProperty(word.toLowerCase())) {
       chain.push(word);
-      if (chain.length > minLength && (this.terminals.hasOwnProperty(word) && !this.isBannedTerminal(word))) {
-        break;
-      }
+      let possibleNextWords = this.wordStats[word.toLowerCase()];
+      word = this.choice(possibleNextWords);
     }
     if (this.props.input.includes(chain.join(' '))) {
-      return this.makeChain(minLength);
+      return this.makeChain(minLength, deepness + 1);
     }
     if (chain.length < minLength) {
-      return this.makeChain(minLength);
+      return this.makeChain(minLength, deepness + 1);
     }
     return chain.join(' ');
   }
